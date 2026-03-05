41926995a1a8897ed4aa5f306bda66de5f0d43b2
sushin <141982169+Sushincm@users.noreply.github.com>
2026-03-05 15:21:31 +0530

    Delete js directory

--- js/js.wordbywordAnimation.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.blink-para').forEach(para => {
-  const text = para.textContent.trim();
-  const words = text.split(' ');
-  para.innerHTML = words.map((word, i) =>
-    `<span style="animation-delay:${i * 0.07}s">${word} </span>`
-  ).join('');
-});
